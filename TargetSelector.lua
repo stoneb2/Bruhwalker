f18c73587ab9f50af7e82ee36f465c1e555a4631
stoneb2 <46829778+stoneb2@users.noreply.github.com>
2021-07-23 22:19:53 -0500

    Update TargetSelector.lua

--- TargetSelector.lua
+++ TargetSelector.lua
@@ -1,5 +1,9 @@
 --[[
 
+--------------------------------------------------------------------------------------------
+
+PLANNING
+
 Modes:
 - One shot
 - Range
@@ -11,12 +15,124 @@ Modes:
 Settings:
 - Range extension / shorten
 - Overkill
-- 
 
 Functions:
 - Priority targetting
 - Target swap if priority is out of range
 - Collision checks
+
+--------------------------------------------------------------------------------------------
+
+API
+
+get_target() [unit]
+- Returns the highest priority target, regardless of collision
+
+get_prio_list() [table of units]
+- Returns the list of targets, ordered by priority (1st in table is highest)
+
+get_q_targets() [table of units]
+- Returns the list of targets that can be hit by Q (if spell data is provided), ordered by 
+  priority (1st in table is highest)
+
+get_q_target() [unit]
+- Returns the highest priority target that can be hit by Q (if spell data is provided), ordered
+  by priority (1st in table is highest)
+
+get_w_targets() [table of units]
+- Returns the list of targets that can be hit by W (if spell data is provided), ordered by 
+  priority (1st in table is highest)
+
+get_w_target() [unit]
+- Returns the highest priority target that can be hit by W (if spell data is provided), ordered
+  by priority (1st in table is highest)
+
+get_e_targets() [table of units]
+- Returns the list of targets that can be hit by E (if spell data is provided), ordered by 
+  priority (1st in table is highest)
+
+get_e_target() [unit]
+- Returns the highest priority target that can be hit by E (if spell data is provided), ordered
+  by priority (1st in table is highest)
+
+get_r_targets() [table of units]
+- Returns the list of targets that can be hit by R (if spell data is provided), ordered by 
+  priority (1st in table is highest)
+
+get_r_target() [unit]
+- Returns the highest priority target that can be hit by R (if spell data is provided), ordered
+  by priority (1st in table is highest)
+
+--------------------------------------------------------------------------------------------
+
+EXAMPLE
+
+Returning a priority list of targets for Ezreal Q, moving to the first target able to be hit
+if Q on main priority has collisions:
+
+ml = require "VectorMath"
+
+local myHero = game.local_player
+local arkpred = _G.Prediction
+
+local q_input = {
+    source = myHero,
+    speed = 2000,
+    range = 1150,
+    delay = 0.25,
+    radius = 60,
+    collision = {"minion", "wind_wall"},
+    type = "linear",
+    hitbox = true
+}
+
+local TS = _G.TS(range = q_input.range, q = q_input) -- might be just _G.TS who tf knows
+
+local function GetFirst(tab)
+    if not next(tab) == nil then
+        if tab[0] ~= nil then
+            return tab[0]
+        else
+            return tab[1]
+        end
+    else
+        return nil
+    end
+end
+
+local function on_tick()
+    if ml.Ready(SLOT_Q) then
+        priority_list = TS:get_prio_list() -- total priority list (not taking collisions into account)
+        q_target_list = TS:get_q_targets() -- priority list that can be hit by Q (no collisions)
+        if GetFirst(priority_list) == GetFirst(q_target_list) then
+            target = GetFirst(priority_list) -- uses top priority target since he can be hit by Q
+            if target then
+                if target.is_valid and target.is_enemy then
+                    local output = arkpred:get_prediction(q_input, target)
+                    local inv = arkpred:get_invisible_duration(target)
+                    if output.hit_chance > 0.5 and inv < (q_input.delay / 2) then
+                        local p = output.cast_pos
+                        spellbook:cast_spell(SLOT_Q, q_input.delay, p.x, p.y, p.z)
+                    end
+                end
+            end
+        else
+            target = GetFirst(q_target_list) -- top priority can't be hit by Q, chooses highest priority target that can be hit by Q
+            if target then
+                if target.is_valid and target.is_enemy then
+                    local output = arkpred:get_prediction(q_input, target)
+                    local inv = arkpred:get_invisible_duration(target)
+                    if output.hit_chance > 0.5 and inv < (q_input.delay / 2) then
+                        local p = output.cast_pos
+                        spellbook:cast_spell(SLOT_Q, q_input.delay, p.x, p.y, p.z)
+                    end
+                end
+            end
+        end
+    end
+end
+
+client:set_event_callback("on_tick", on_tick)
 
 --]]
 
